dbc6f8790306c0715c130069f426282bc009e993
a20140097 <131193791+a20140097@users.noreply.github.com>
2023-09-19 12:43:13 -0500

    Add files via upload

---
+++ cuestionario.js
@@ -0,0 +1,51 @@
+var nombre = prompt("Esta es la Trivia de la primavera. Escribe tu nombre:");
+var c = 0:
+var i = 0;
+alert("Bienvenido/" + nombre + "EMPEZAMOS. . .")
+
+var p1 = prompt("¿Cómo se llama al primer día de la Primavera?\nA: ¿Cómo se llama al primer día de la Primavera?\nB: ¿Cómo se llama al primer día de la Primavera?\nC:
+A: 6 meses de luz diurna ininterrumpida
+B: 6 meses de oscuridad inimterrumpida
+C: 6 meses de alternancia de luz y oscuridad")
+
+if(p1 == "B"){
+    c = c + 1;
+}else if (p1 == "A"){
+    i = + 1;
+}else if (p1 == "C"){
+    i = i + 1;
+}else{
+    alert}("Opción no valida")
+}
+var p1 = prompt("¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA: A: 6 meses de luz diurna ininterrumpida
+B: 6 meses de oscuridad inimterrumpida
+C: 6 meses de alternancia de luz y oscuridad
+
+\nA: ¿Cómo se llama al primer día de la Primavera?\nB: ¿Cómo se llama al primer día de la Primavera?\nC:
+A: 6 meses de luz diurna ininterrumpida
+B: 6 meses de oscuridad inimterrumpida
+C: 6 meses de alternancia de luz y oscuridad")
+
+if(p2 == "B"){
+    c = c + 1;
+}else if (p2 == "A"){
+    i = + 1;
+}else if (p2 == "C"){
+    i = i + 1;
+}else{
+    alert}("Opción no valida")
+}
+if{c > i){
+    document.write(nombre + ", lo has hecho bien<br>");
+    document.write("Coreectas:"+ c + "<br>");
+    document.write("Incorrectas:"+ i + "<br>");
+
+}else if (c < i){
+    document.write(nombre + ", lo has hecho bien<br>");
+    document.write("Coreectas:"+ c + "<br>");
+    document.write("Incorrectas:"+ i + "<br>");
+}else{
+    document.write(nombre + ", lo has hecho bien<br>");
+    document.write("Coreectas:"+ c + "<br>");
+    document.write("Incorrectas:"+ i + "<br>");
+}
